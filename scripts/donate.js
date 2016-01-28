da3268a2fdac5cdd6fbf9794233122262b86e5c8
root <slimsheivy@aol.com>
2016-01-28 15:56:43 -0500

    added validation for donate form

--- scripts/donate.js
+++ scripts/donate.js
@@ -1,12 +1,79 @@
 $(document).ready(function () {
 
+	var luhnChk=function(a){return function(c){for(var l=c.length,b=1,s=0,v;l;)v=parseInt(c.charAt(--l),10),s+=(b^=1)?a[v]:v;return s&&0===s%10}}([0,2,4,6,8,1,3,5,7,9]);
+
 	braintree.setup($('.client-token').val(), "custom", {id: "checkout"});
 
-	$('.card-number').payment('formatCardNumber');
-//	$('.expiration').payment('formatCardExpiry');
-	$('.cvv').payment('formatCardCVC');
+	$('#card-number input').payment('formatCardNumber');
+	$('#cvv input').payment('formatCardCVC');
 
+	$('#profile__form #submit').click(function(){
+		var bad = false;
+		
+		$('.error-msg').each(function(index){
+			bad = bad || $(this).text().trim().length > 0;
+		});
+		return !bad;
+	});
 
+	$('#card-number input').focusout(function(){
+ 		var val = $(this).val().replace(/\s/g, '');
+		
+		if(!luhnChk(val))
+			$(this).next().text('Please enter a valid credit card number');
+		else
+			$(this).next().text('');
+	});
+
+	$('#expiration input').focusout(function(){
+ 		var val = $(this).val().trim();
+		var regex = /^\d{2}\/(\d{2}|\d{4})$/;
+
+		if(!val.match(regex))
+			$(this).next().text('Please enter a valid expiration date (e.g. 11/20)');
+		else
+			$(this).next().text('');
+	});
+
+	$('#state input').focusout(function(){
+ 		var val = $(this).val().trim();
+		var regex = /^[a-zA-Z]{2}$/;
+
+		if(!val.match(regex))
+			$(this).next().text('Please enter a valid state (e.g. NY)');
+		else
+			$(this).next().text('');
+	});
+
+	$('#postal input').focusout(function(){
+ 		var val = $(this).val().trim();
+		var regex = /^\d{5}$/;
+
+		if(!val.match(regex))
+			$(this).next().text('Please enter a valid zip code (e.g. 11213)');
+		else
+			$(this).next().text('');
+	});
+
+	$('#email input').focusout(function(){
+ 		var val = $(this).val().trim();
+		var regex = /^\S{2,}@\S{2,}\.[a-zA-Z]{2,10}(\.[a-zA-Z]{2,10})?$/;
+
+		if(!val.match(regex))
+			$(this).next().text('Please enter a valid email (e.g. info@tiferesrachamim.com)');
+		else
+			$(this).next().text('');
+	});
+
+	$('#amount input').focusout(function(){
+ 		var val = $(this).val().trim();
+		var regex = /^\d{1,5}(\.\d{2})?$/;
+
+		if(!val.match(regex))
+			$(this).next().text('Please enter a valid amount (e.g. 180.00)');
+		else
+			$(this).next().text('');
+	});
 
     	var tz = jstz.determine(); // Determines the time zone of the browser client
     	$('.client-tz').val(tz.name()); //'Asia/Kolhata' for Indian Time.
--- views/donate.php
+++ views/donate.php
@@ -1,37 +1,74 @@
 <?php
 	require_once('include/braintree.php');
 ?>	 
-	<div class="wrap">
-	    <h1>Donate to Tiferes Rachamim</h1>
-            <div class="clear"></div>
+  <div class="wrap">
 
-            <form id="checkout" action="/checkout" method="post" class="profile__form">
-               <div class="profile__fields">
-  	  	  <input class="input card-number" data-braintree-name="number" placeholder="Credit Card Number" required>
+    <h1>Donate to Tiferes Rachamim</h1>
+    <div class="clear"></div>
 
-  	          <input class="input cvv" data-braintree-name="cvv" placeholder="Security Code" required>
-	  	  <input class="input expiration" data-braintree-name="expiration_date" placeholder="Expiration (mm/yy)" pattern="\d{2}/(\d{2}|\d{4})" required>
+    <form id="checkout" action="/checkout" method="post" class="profile__form">
+      <div class="profile__fields">
 
-	  	  <input class="input " data-braintree-name="cardholder_name" placeholder="Cardholder Name" required>
-	  	  <input class="input" data-braintree-name="street_address" placeholder="Address" required>
-	  	  <input class="input city" data-braintree-name="locality" placeholder="City" required>
-	  	  <input class="input state" data-braintree-name="region" placeholder="State (e.g. NY)" pattern="[a-zA-Z]{2}" required>
-	  	  <input class="input postal" data-braintree-name="postal_code" placeholder="Zip" pattern="\d{5}" required>
-	  	  <input class="input" name="email" placeholder="Email Address" type="email" required>
+        <div class="field" id="card-number">
+          <input class="input" data-braintree-name="number" placeholder="Credit Card Number" required>
+          <div class="error-msg"></div>
+        </div>
 
-		  <input class="input" name="amount" placeholder="Amount" autocomplete="off" pattern="\d{1,5}(.\d{2})?" equired>
-		  <div id="errors">
+        <div class="field" id="cvv">
+          <input class="input" data-braintree-name="cvv" placeholder="Security Code" required>
+          <div class="error-msg"></div>
+        </div>
 
-		  </div>
+        <div class="field" id="expiration">
+          <input class="input" data-braintree-name="expiration_date" placeholder="Expiration (mm/yy)" required>
+          <div class="error-msg"></div>
+        </div>
+
+        <div class="field" id="card-name">
+          <input class="input" data-braintree-name="cardholder_name" placeholder="Cardholder Name" required>
+          <div class="error-msg"></div>
+        </div>
+
+        <div class="field" id="address">
+          <input class="input" data-braintree-name="street_address" placeholder="Address" required>
+          <div class="error-msg"></div>
+        </div>
+
+        <div class="field" id="city">
+          <input class="input" data-braintree-name="locality" placeholder="City" required>
+          <div class="error-msg"></div>
+        </div>
+
+        <div class="field" id="state">
+          <input class="input" data-braintree-name="region" placeholder="State (e.g. NY)" required>
+          <div class="error-msg"></div>
+        </div>
+
+        <div class="field" id="postal">
+          <input class="input" data-braintree-name="postal_code" placeholder="Zip" required>
+          <div class="error-msg"></div>
+        </div>
+
+        <div class="field" id="email">
+          <input class="input" name="email" placeholder="Email Address" required>
+          <div class="error-msg"></div>
+        </div>
+
+        <div class="field" id="amount">
+          <input class="input" name="amount" placeholder="Amount" autocomplete="off" required>
+          <div class="error-msg"></div>
+        </div>
 <?php
 	echo "<input class='client-token' type='hidden' value='".Braintree_ClientToken::generate()."'>";
 ?>
-		<input name='client-tz' class='client-tz' type='hidden'>
-		  <div class="profile__footer">
-		     <h3>Please click the Donate button only once</h3> 
-            	     <div class="clear"></div>
-		     <input class="btn" type="submit" id="submit" value="Donate">
-		  </div>
-	       </div>
-	    </form>
-         </div>
+	<input name='client-tz' class='client-tz' type='hidden'>
+
+      </div>
+
+      <div class="profile__footer">
+        <div class="disclaimer">Please click the Donate button only once</div>
+        <div class="clear"></div>
+        <input class="btn" type="submit" id="submit" value="Donate">
+      </div>
+  </form>
+</div>
