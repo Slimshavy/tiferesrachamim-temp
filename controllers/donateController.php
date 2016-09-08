ccee5205eb39f4434b356086379d51e20f2057d8
root <slimsheivy@aol.com>
2016-09-08 15:03:04 -0400

    forward donate page to donate.tiferesrachamim.com

--- controllers/donateController.php
+++ controllers/donateController.php
@@ -1,13 +1,5 @@
 <?php
-	$controller = array(
-		'title' => 'Donate',
-		'view' => 'donate',
-		'styles' => array('donate'),
-		'scripts' => array('jquery','timezonedetector','jquery.payment.min','donate','braintree')
-	);
-	
-	if($config['devlopment'] == true)
-		$controller['scripts'][] = 'donatetest';
-
-	return $controller;
+	//header("Location: donate.tiferesrachamim.com");
+	header("Location: http://donate.tiferesrachamim.com");
+	exit();
 ?>
